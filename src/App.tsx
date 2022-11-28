bcfd4b1df41feb0cef7de9fc54c9484abba4121c
oleksii vovnenko <kespa71for@gmail.com>
2022-11-28 16:08:30 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
+interface Todo {
+  "userId": number,
+  "id": number,
+  "title": string,
+  "completed": boolean
+}
 
 function App() {
+  const todo = 'https://jsonplaceholder.typicode.com/todos/'
+
+  //useState
+  const [state] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  //useEffect
+  useEffect(() => {
+    axios.get(todo)
+        .then(response => setTodos(response.data))
+  }, [])
+
   return (
     <div className="App">
-
+      {state}
+      {todos.map(element => {
+        return <li key={element.id}>{element.title}</li>
+      })}
     </div>
   );
 }
